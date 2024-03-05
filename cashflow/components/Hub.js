d8030b14d637eb4a5f110fe5ee3ceb47856e3809
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-03-05 14:40:51 -0300

    hub metadinha

--- cashflow/components/Hub.js
+++ cashflow/components/Hub.js
@@ -1,17 +1,20 @@
-import {View, StyleSheet, Text} from "react-native";
+import {View, StyleSheet, Text, Image} from "react-native";
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faBell, faEye} from "@fortawesome/free-solid-svg-icons";
 
 export default function (){
     return(
             <View style={css.hub}>
                 <View style={css.user}>
                     <View>
-                        <Text style={css.nome}>Murilo Marques</Text>
+                        <Text style={css.textohub}>Murilo Marques</Text>
                     </View>
-
+                    <FontAwesomeIcon size={25} color={'white'} icon={faBell} />
                 </View>
                 <View style={css.dinheiro}>
-                    <Text>R$ 1723,10</Text>
+                    <Text style={css.valor}>R$ 1723,10</Text>
+                    <FontAwesomeIcon color={'white'} size={35}  icon={faEye} />
                 </View>
             </View>
     )
@@ -27,11 +30,27 @@ const css = StyleSheet.create({
     },
     user: {
         height: '30%',
-        justifyContent: 'space-around',
-        paddingLeft: 20
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent: 'space-between',
+        paddingLeft: 20,
+        paddingRight: 20
     },
-    nome: {
+    valor: {
+        fontSize: 35,
+        color: 'white',
+        fontWeight:'bold'
+    },
+    textohub: {
         fontSize: 20,
+        color: 'white',
         fontWeight: 'bold'
+    },
+    dinheiro: {
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent: 'center',
+        paddingTop:25,
+        gap:10
     }
 })
