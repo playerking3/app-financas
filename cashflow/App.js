a39ec1fe62f639fa2d84a8d60968fb5566891eb9
Murilo  M. Castro <123572431+playerking3@users.noreply.github.com>
2024-03-05 10:42:18 -0300

    paginas consertadas

--- cashflow/App.js
+++ cashflow/App.js
@@ -1,11 +1,13 @@
+import {NavigationContainer} from "@react-navigation/native";
 import CreateStackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
+import Listagem from "./screens/Listagem";
 
-stack =CreateStackNavigator()
+Stack = CreateStackNavigator()
 export default function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator>
-          {/*<Stack.Screen name=""component={}/>*/}
+          <Stack.Screen name="Listagem" component={Listagem}/>
         </Stack.Navigator>
       </NavigationContainer>
   );
