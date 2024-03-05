25e8bd34628619b39e3cff8e89feec2b9f10894c
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-03-05 11:27:29 -0300

    amarelo

--- cashflow/screens/Home.js
+++ cashflow/screens/Home.js
@@ -1,9 +1,21 @@
-import {SafeAreaView} from "react-native";
+import {View, StyleSheet, Text} from "react-native";
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function (){
     return(
         <SafeAreaView>
+            <View style={css.hub}>
 
+            </View>
         </SafeAreaView>
     )
 }
+
+const css = StyleSheet.create({
+    hub: {
+        height:'30%',
+        width:'100%',
+        backgroundColor: '#FFD60A'
+
+    }
+})
