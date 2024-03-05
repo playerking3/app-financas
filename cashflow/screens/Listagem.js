4da11ca0952609ba498e216531f5a6536ec66c09
Murilo  M. Castro <123572431+playerking3@users.noreply.github.com>
2024-03-05 14:12:56 -0300

    coloquei o fontawesome

--- cashflow/screens/Listagem.js
+++ cashflow/screens/Listagem.js
@@ -1,9 +1,31 @@
-import {SafeAreaView} from "react-native";
+import {SafeAreaView, StyleSheet, Text, View} from "react-native";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faXmark} from "@fortawesome/free-solid-svg-icons/faXmark";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
 
 export default function (){
     return(
         <SafeAreaView>
-
+            <View style={css.selectorDisplay}>
+                <Text>Minhas economias</Text>
+                <FontAwesomeIcon icon={faPlus} />
+            </View>
+            <View style={css.selectorDisplay}>
+                <Text>Minhas despesas</Text>
+                <FontAwesomeIcon icon={faXmark} style={css.iconBg} />
+            </View>
         </SafeAreaView>
     )
 }
+
+const css = StyleSheet.create({
+    selectorDisplay: {
+        justifyContent: "space-between",
+        flexDirection: "row"
+    },
+    iconBg: {
+        color: 'white',
+        backgroundColor: '#FFD60A',
+        borderRadius: 12
+    }
+})
