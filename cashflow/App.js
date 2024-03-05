cfbdb176391e69b3857f53a60b0d9ac3d2b7a9d7
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-03-05 11:16:35 -0300

    home existindo

--- cashflow/App.js
+++ cashflow/App.js
@@ -1,13 +1,15 @@
 import {NavigationContainer} from "@react-navigation/native";
 import CreateStackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
 import Listagem from "./screens/Listagem";
+import Home from "./screens/Home";
 
 Stack = CreateStackNavigator()
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Listagem" component={Listagem}/>
+        <Stack.Navigator initialRouteName={'Home'} screenOptions={{ headerShown: false}}>
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Listagem" component={Listagem}/>
         </Stack.Navigator>
       </NavigationContainer>
   );
