c5ced451e1f03fdacac25a929c51d01e0399c22f
Murilo  M. Castro <123572431+playerking3@users.noreply.github.com>
2024-03-12 14:32:11 -0300

    biometria feita

--- cashflow/screens/Start.js
+++ cashflow/screens/Start.js
@@ -1,6 +1,16 @@
 import {View, StyleSheet, Text, Image, Pressable} from "react-native";
 import {css} from '../Style/StyleStart'
+import * as LocalAuthentication from 'expo-local-authentication';
 function Start({navigation}){
+    async function autenticar(){
+         const resposta = await LocalAuthentication.authenticateAsync()
+        console.log(resposta)
+        if (resposta.success){
+            console.log("aaaaaa")
+            navigation.navigate("Home")
+        }
+    }
+
     return(
         <View style={css.container}>
             <View style={css.logo}>
@@ -10,7 +20,7 @@ function Start({navigation}){
                 <Image source={require('../assets/Vector.png')}></Image>
             </View>
             <View style={css.entrar}>
-                <Pressable onPress={() => navigation.navigate("Home")} style={css.btnEntrar}>
+                <Pressable onPress={autenticar} style={css.btnEntrar}>
                     <Text style={css.textEntrar}>
                         Entrar
                     </Text>
