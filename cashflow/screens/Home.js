95e566b269ed4a7e4b5070c0135db019136eec8b
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2024-03-19 08:05:11 -0300

    useContext

--- cashflow/screens/Home.js
+++ cashflow/screens/Home.js
@@ -5,8 +5,13 @@ import Metas from "../components/Metas";
 import CardHome from "../components/CardHome";
 import Navigator from "../components/Navigator";
 import {css} from '../Style/StyleHome'
+import {useContext} from "react";
+import {Dados} from "../context/Dados";
 
 export default function ({navigation}){
+    // const {financeiro, cadastroFinanceiro} = useContext(Dados)
+
+
     return(
         <SafeAreaView style={css.container}>
             <Hub></Hub>
