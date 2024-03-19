6a05b2962464212e8ca7e81a9abe09a3d195fd8d
Murilo  M. Castro <123572431+playerking3@users.noreply.github.com>
2024-03-19 11:10:26 -0300

    tabela modificada SOLID

--- cashflow/context/Dados.js
+++ cashflow/context/Dados.js
@@ -1,11 +1,20 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import {Alert} from "react-native";
 
 const Dados = createContext();
 
 function DadosProvider({ children }) {
     const [financeiro, setFinanceiro] = useState([])
-    //const [total, setTotal] =
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        let aux = 0
+        financeiro.map((item)=>{
+            aux += item.valor
+        })
+        setTotal(aux)
+    }, [financeiro]);
+
     function cadastroFinanceiro(dados = {}) {
         if(dados?.valor === "" || dados?.nome === "" || dados?.data === "" || dados?.categoria === "") {
             return Alert.alert("Dados não informados")
