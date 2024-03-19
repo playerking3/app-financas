574ac1d5a6c6165ebd7641f48f4921ee0f57a0dd
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-03-19 08:24:02 -0300

    config existe

--- cashflow/screens/Config.js
+++ cashflow/screens/Config.js
@@ -1,15 +1,19 @@
-import {View, StyleSheet, Text} from "react-native";
+import {View, StyleSheet, Text, Pressable} from "react-native";
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Hub from "../components/Hub";
 import Navigator from "../components/Navigator";
-import {css} from '../Style/StyleHome'
+import {css} from '../Style/StyleConfig'
 
 export default function ({navigation}){
     return(
         <SafeAreaView style={css.container}>
             <Hub></Hub>
-            <View>
-
+            <View style={css.divgrande}>
+                <Pressable style={css.btnDark}>
+                    <Text style={css.btnText}>
+                        Modo Escuro
+                    </Text>
+                </Pressable>
             </View>
             <Navigator navigation={navigation}></Navigator>
         </SafeAreaView>
